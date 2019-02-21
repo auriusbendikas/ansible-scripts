cdf3607c22fd8076781627a581f10ffb736982f9
Aurius Bendikas <aurius.bendikas@gmail.com>
2019-02-21 21:28:42 +0100

    EFI signing support.

--- bin/run-playbook.sh
+++ bin/run-playbook.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(readlink -f $0 | xargs dirname)
-env ANSIBLE_CONFIG=$SCRIPT_DIR/ansible.cfg ansible-playbook --inventory $SCRIPT_DIR/inventory.yaml --connection=local $1
+PROPERTIES_FILE=$(readlink -f $SCRIPT_DIR | xargs dirname)/properties/$(basename -- $1)
+env ANSIBLE_CONFIG=$SCRIPT_DIR/ansible.cfg ansible-playbook --inventory $SCRIPT_DIR/inventory.yaml --extra-vars @$PROPERTIES_FILE --connection=local $1
