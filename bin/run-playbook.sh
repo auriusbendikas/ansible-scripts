28a6f0018384f5bc25aba927af6079a9dead6df2
Aurius Bendikas <aurius.bendikas@gmail.com>
2019-02-24 19:03:56 +0100

    Install i3

--- bin/run-playbook.sh
+++ bin/run-playbook.sh
@@ -2,4 +2,11 @@
 
 SCRIPT_DIR=$(readlink -f $0 | xargs dirname)
 PROPERTIES_FILE=$(readlink -f $SCRIPT_DIR | xargs dirname)/properties/$(basename -- $1)
-env ANSIBLE_CONFIG=$SCRIPT_DIR/ansible.cfg ansible-playbook --inventory $SCRIPT_DIR/inventory.yaml --extra-vars @$PROPERTIES_FILE --connection=local $1
+
+if [ $EUID != 0 ]; then
+    ASK_BECOME_PASSWORD=--ask-become-pass
+else
+    ASK_BECOME_PASSWORD=
+fi
+
+env ANSIBLE_CONFIG=$SCRIPT_DIR/ansible.cfg ansible-playbook --inventory $SCRIPT_DIR/inventory.yaml --extra-vars @$PROPERTIES_FILE $ASK_BECOME_PASSWORD --connection=local $1
