73fa8496bd6ee9c98860a90a629c0c68f881cad9
Aurius Bendikas Chang <aurius.bendikas@gmail.com>
2022-04-27 12:55:47 +0200

    Added git alias pull-masters-parallel

--- roles/git/templates/etc/profile.d/git-aliases.sh
+++ roles/git/templates/etc/profile.d/git-aliases.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-alias pull-masters="find -name .git -type d -printf '\033[0;33m%h:\033[0m\n' -execdir git fetch origin \; -execdir git --no-pager diff --stat master origin/master \; -execdir git update-ref refs/heads/master origin/master \; -execdir bash -c 'if [[ \$(git symbolic-ref --short HEAD) == master ]] ; then git reset --hard HEAD ; fi' \;"
+alias pull-masters='find -name .git -type d -printf "\033[0;33m%h:\033[0m\n" -execdir bash -c "git fetch --quiet origin && git --no-pager diff --color=always --stat master origin/master && git update-ref refs/heads/master origin/master && if [[ \$(git symbolic-ref --short HEAD) == master ]] ; then git reset --hard HEAD ; fi" \;'
+alias pull-masters-parallel=$'find -name .git -type d -printf "%h\\0" | xargs -0 -P 0 -I {} bash -c "(cd {} && git fetch --quiet origin && git --no-pager diff --color=always --stat master origin/master && git update-ref refs/heads/master origin/master && if [[ \$(git symbolic-ref --short HEAD) == master ]] ; then git reset --hard HEAD ; fi) > >(awk \'\$0=\\"\x1b[0;33m{}: \x1b[0m\\"\$0\') 2> >(awk \'\$0=\\"\x1b[0;31m{}: \x1b[0m\\"\$0\' >&2)"'
